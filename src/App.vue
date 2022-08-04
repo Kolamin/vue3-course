f6b5f20e0010ec6b6d6e0a72ad88d576258a9671
Anton Zhiliakov <gofranich@gmail.com>
2022-08-04 10:45:02 +0600

    Add fetch function

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,89 @@
 <template>
-    <div class="app">
-        <h1>Страница с постами</h1>
-        <my-button
-                @click="showDialog"
-                style="margin: 15px 0;"
-        >Создать пользователя</my-button>
-        <my-dialog v-model:show="dialogVisible">
-            <post-form
-                    @create="createPost"
-            />
-        </my-dialog>
-        <post-list
-                :posts="posts"
-                @remove="removePost"
-        >
-
-        </post-list>
-    </div>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-button
+        @click="showDialog"
+        style="margin: 15px 0;"
+    >Создать пользователя
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+    <post-list
+        :posts="posts"
+        @remove="removePost"
+        v-if="!isPostsLoading"
+    >
+    </post-list>
+    <div v-else>Идет загрузка...</div>
+  </div>
 </template>
 
 <script>
-    import PostForm from "@/components/PostForm";
-    import PostList from "@/components/PostList";
-    import MyDialog from "./components/UI/MyDialog";
-    import MyButton from "./components/UI/MyButton";
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyDialog from "./components/UI/MyDialog";
+import MyButton from "./components/UI/MyButton";
+import axios from "axios";
 
-    export default {
-        components: {
-            MyButton,
-            MyDialog,
-            PostList, PostForm
-        },
-        data() {
-            return {
-                posts: [
-                    {id: 1, title: 'JavaScript', body: 'Описание поста'},
-                    {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
-                    {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
-                    {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
-                ],
-                dialogVisible: false
-            }
-        },
-        methods: {
-            createPost(post) {
-                this.posts.push(post)
-                this.dialogVisible = false
-            },
-            removePost(post) {
-                this.posts = this.posts.filter(p => p.id !== post.id)
-            },
-            showDialog(){
-                this.dialogVisible = true
-            }
-        }
+export default {
+  components: {
+    MyButton,
+    MyDialog,
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false
     }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
+    async fetchPosts() {
+      this.isPostsLoading = true
+      try {
+        const response = await axios
+            .get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data
+        this.isPostsLoading = false
+      } catch (e) {
+        alert('Error')
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+  }
+}
 </script>
 
 <style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-    form {
-        display: flex;
-        flex-direction: column;
-    }
+form {
+  display: flex;
+  flex-direction: column;
+}
 
-    .app {
-        padding: 20px;
-    }
+.app {
+  padding: 20px;
+}
 
 </style>
